feb405e6fef1ec9992e92e63ee1ae8229de03d32
Alexandr <you@example.com>
2022-11-17 22:46:23 +0300

    del api

--- store/api/card.js
+++
@@ -1,6 +0,0 @@
-import _paramsCard from './paramsCard.json'
-const TIMEOUT = 100
-export default {
-    getProducts: (cb, timeout) => setTimeout(() => cb(_paramsCard), timeout || TIMEOUT),
-    buyProducts: (payload, cb, timeout) => setTimeout(() => cb(), timeout || TIMEOUT)
-}
